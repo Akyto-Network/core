de8b954d7b99796d2f06ff1813a16a0da23c5964
Victor <47831936+VictorEgret@users.noreply.github.com>
2024-05-10 01:20:20 +0200

    Rgb Armor Task

--- src/main/java/gym/core/rank/RankEntry.java
+++ src/main/java/gym/core/rank/RankEntry.java
@@ -12,15 +12,23 @@ public class RankEntry {
 	private String prefix;
 	public String getPrefix() { return Utils.translate(this.prefix); }
 	private String color;
-	private Boolean hasSpaceBetweenColor;
+	private Boolean spaceBetweenColor;
 	private RankType rankType;
 	private List<String> permissions;
 	
 	public RankEntry(final String prefix, final String color, final Boolean spacer, final List<String> permissions, final RankType type) {
 		this.prefix = prefix;
 		this.color = color;
-		this.hasSpaceBetweenColor = spacer;
+		this.spaceBetweenColor = spacer;
 		this.rankType = type;
 		this.permissions = permissions;
+	}
+
+	public Boolean hasSpaceBetweenColor() {
+		return this.spaceBetweenColor;
+	}
+
+	public boolean isStaff() {
+		return this.rankType.ordinal() > RankType.COMMUNITY.ordinal();
 	}
 }
