83c9320ea9879c5d1489dc264554c75822e89ac9
» Kezuk <54121447+kezukdev@users.noreply.github.com>
2024-05-10 22:58:43 +0200

    split cmd.getName into two function

--- src/main/java/gym/core/handler/command/MessageCommand.java
+++ src/main/java/gym/core/handler/command/MessageCommand.java
@@ -3,7 +3,6 @@ package gym.core.handler.command;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import gym.core.Core;
@@ -18,81 +17,85 @@ public class MessageCommand {
 
 	@Command(name = "msg", aliases= {"message", "tell", "whisper", "r", "m", "reply"}, inGameOnly = true)
 	public void messageCommand(final CommandArgs arg) {
-		final CommandSender sender = arg.getSender();
+		final Player sender = arg.getPlayer();
 		final String[] args = arg.getArgs();
-		final org.bukkit.command.Command cmd = arg.getCommand();
-		if (!(sender instanceof Player)) return;
 		if (args.length == 0) {
-			sender.sendMessage(ChatColor.RED + "/" + cmd.getName() + (cmd.getName().contains("r") ? "<message>" : "<player> <message>"));
+			sender.sendMessage(ChatColor.RED + "/msg <player> <message>");
 			return;
 		}
-		if (cmd.getName().contains("m")) {
-			if (Bukkit.getPlayer(args[0]) == null) {
-				sender.sendMessage(ChatColor.RED + "Target isn't connected!");
-				return;
-			}
-			final Profile senderProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(Bukkit.getPlayer(sender.getName()).getUniqueId());
-			final Profile receiverProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(Bukkit.getPlayer(args[0]).getUniqueId());
-			String msg = args.length > 1 ? StringUtils.join(args, ' ', 1, args.length) : "Hi, how are you today?"; 
-			sender.sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
-					.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
-					.replace("%sender%", sender.getName())
-					.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
-					.replace("%receiver%", args[0])
-					.replace("%incommingType%", "To")
-					.replace("%player%", args[0])
-					.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
-					.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
-					.replace("%message%", msg));
-			Bukkit.getPlayer(args[0]).sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
-					.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
-					.replace("%sender%", sender.getName())
-					.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
-					.replace("%receiver%", args[0])
-					.replace("%incommingType%", "From")
-					.replace("%player%", sender.getName())
-					.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
-					.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
-					.replace("%message%", msg));
-			receiverProfile.setResponsive(Bukkit.getPlayer(sender.getName()).getUniqueId());
+		if (Bukkit.getPlayer(args[0]) == null) {
+			sender.sendMessage(ChatColor.RED + "Target isn't connected!");
 			return;
 		}
-		if (cmd.getName().contains("r")) {
-			final Profile senderProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(Bukkit.getPlayer(sender.getName()).getUniqueId());
-			if (senderProfile.getResponsive() == null) {
-				sender.sendMessage(ChatColor.RED + "You doesn't have anyone to answer.");
-				return;
-			}
-			if (Bukkit.getPlayer(senderProfile.getResponsive()) == null) {
-				sender.sendMessage(ChatColor.RED + "Target isn't connected!");
-				return;
-			}
-			final Profile receiverProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(senderProfile.getResponsive());
-			String msg = args.length > 0 ? StringUtils.join(args, ' ', 0, args.length) : "Hi, how are you today?"; 
-			sender.sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
-					.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
-					.replace("%sender%", sender.getName())
-					.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
-					.replace("%receiver%", Bukkit.getPlayer(senderProfile.getResponsive()).getName())
-					.replace("%incommingType%", "To")
-					.replace("%player%", Bukkit.getPlayer(senderProfile.getResponsive()).getName())
-					.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
-					.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
-					.replace("%message%", msg));
-			Bukkit.getPlayer(senderProfile.getResponsive()).sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
-					.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
-					.replace("%sender%", sender.getName())
-					.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
-					.replace("%receiver%", Bukkit.getPlayer(senderProfile.getResponsive()).getName())
-					.replace("%incommingType%", "From")
-					.replace("%player%", sender.getName())
-					.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
-					.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
-					.replace("%message%", msg));
-			receiverProfile.setResponsive(Bukkit.getPlayer(sender.getName()).getUniqueId());
-			return;
-		}
+		final Profile senderProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(sender.getUniqueId());
+		final Profile receiverProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(Bukkit.getPlayer(args[0]).getUniqueId());
+		String msg = args.length > 1 ? StringUtils.join(args, ' ', 1, args.length) : "Hi, how are you today?"; 
+		sender.sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
+				.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
+				.replace("%sender%", sender.getName())
+				.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
+				.replace("%receiver%", args[0])
+				.replace("%incommingType%", "To")
+				.replace("%player%", args[0])
+				.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
+				.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
+				.replace("%message%", msg));
+		Bukkit.getPlayer(args[0]).sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
+				.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
+				.replace("%sender%", sender.getName())
+				.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
+				.replace("%receiver%", args[0])
+				.replace("%incommingType%", "From")
+				.replace("%player%", sender.getName())
+				.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
+				.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
+				.replace("%message%", msg));
+		receiverProfile.setResponsive(sender.getUniqueId());
 		return;
 	}
+	
+	@Command(name = "reply", aliases= {"r","answer"}, inGameOnly = true)
+	public void replyCommand(final CommandArgs arg) {
+		final Player sender = arg.getPlayer();
+		final String[] args = arg.getArgs();
+		if (args.length == 0) {
+			sender.sendMessage(ChatColor.RED + "/reply <message>");
+			return;
+		}
+		final Profile senderProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(sender.getUniqueId());
+		if (senderProfile.getResponsive() == null) {
+			sender.sendMessage(ChatColor.RED + "You doesn't have anyone to answer.");
+			return;
+		}
+		if (Bukkit.getPlayer(senderProfile.getResponsive()) == null) {
+			sender.sendMessage(ChatColor.RED + "Target isn't connected!");
+			return;
+		}
+		final Profile receiverProfile = this.main.getManagerHandler().getProfileManager().getProfiles().get(senderProfile.getResponsive());
+		String msg = args.length > 0 ? StringUtils.join(args, ' ', 0, args.length) : "Hi, how are you today?"; 
+		sender.sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
+				.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
+				.replace("%sender%", sender.getName())
+				.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
+				.replace("%receiver%", Bukkit.getPlayer(senderProfile.getResponsive()).getName())
+				.replace("%incommingType%", "To")
+				.replace("%player%", Bukkit.getPlayer(senderProfile.getResponsive()).getName())
+				.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
+				.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
+				.replace("%message%", msg));
+		Bukkit.getPlayer(senderProfile.getResponsive()).sendMessage(this.main.getLoaderHandler().getMessage().getPmFormat()
+				.replace("%senderColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
+				.replace("%sender%", sender.getName())
+				.replace("%receiverColorRank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(receiverProfile.getRank()).getColor()))
+				.replace("%receiver%", Bukkit.getPlayer(senderProfile.getResponsive()).getName())
+				.replace("%incommingType%", "From")
+				.replace("%player%", sender.getName())
+				.replace("%rankColor%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getColor()))
+				.replace("%rank%", Utils.translate(this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).getPrefix()) + (this.main.getManagerHandler().getRankManager().getRanks().get(senderProfile.getRank()).hasSpaceBetweenColor() ? " " : ""))
+				.replace("%message%", msg));
+		receiverProfile.setResponsive(sender.getUniqueId());
+		return;
+	}
+	
 
 }
--- src/main/java/gym/core/handler/command/MuteCommand.java
+++ src/main/java/gym/core/handler/command/MuteCommand.java
@@ -17,11 +17,10 @@ public class MuteCommand {
 	
 	private Core main = Core.API;
 
-	@Command(name = "mute", aliases = {"unmute"}, inGameOnly = false)
-	public void muteCommand(final CommandArgs arg){
+	@Command(name = "mute", inGameOnly = false)
+	public void muteCommand(final CommandArgs arg) {
 		final CommandSender sender = arg.getSender();
 		final String[] args = arg.getArgs();
-		final org.bukkit.command.Command cmd = arg.getCommand();
 		if (!sender.hasPermission(this.main.getLoaderHandler().getPermission().getAddBan())) {
 			sender.sendMessage(this.main.getLoaderHandler().getMessage().getNoPermission());
 			return;
@@ -30,46 +29,46 @@ public class MuteCommand {
 			this.main.getLoaderHandler().getMessage().getMuteHelp().forEach(sender::sendMessage);
 			return;
 		}
-		if (cmd.getName().equalsIgnoreCase("mute")) {
-			if (args.length == 1) {
-				this.main.getLoaderHandler().getMessage().getMuteHelp().forEach(sender::sendMessage);
-				return;
-			}
-			if (args[0].equalsIgnoreCase("info") && args.length == 2) {
-				if (!this.main.getManagerHandler().getPunishmentManager().getMuted().containsKey(Bukkit.getPlayer(args[1]) != null ? Bukkit.getPlayer(args[1]).getUniqueId() : Bukkit.getOfflinePlayer(args[1]).getUniqueId())) {
-					sender.sendMessage(this.main.getLoaderHandler().getMessage().getNotExist().replace("%value%", args[1]).replace("%type%", "mute"));
-					return;
-				}
-				final MuteEntry ban = this.main.getManagerHandler().getPunishmentManager().getMuted().get(Bukkit.getPlayer(args[1]) != null ? Bukkit.getPlayer(args[1]).getUniqueId() : Bukkit.getOfflinePlayer(args[1]).getUniqueId());
-				this.main.getLoaderHandler().getMessage().getPunishmentInfo().forEach(str -> sender.sendMessage(str.replace("%user%", args[1]).replace("%punishmentType%", "MUTE").replace("%expires%", ban.getExpiresOn()).replace("%reason%", ban.getReason()).replace("%judge%", ban.getJudge())));
-				return;
-			}
-	    	Calendar calendar = Calendar.getInstance();
-	    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-	    	String reason = "Flood";
-			new TimeUtils(args[1], calendar);
-            reason = args.length > 2 ? StringUtils.join(args, ' ', 2, args.length) : "Flood";  
-	        this.main.getManagerHandler().getPunishmentManager().addMute(Bukkit.getPlayer(args[0]) != null ? Bukkit.getPlayer(args[0]).getUniqueId() : Bukkit.getOfflinePlayer(args[0]).getUniqueId(), sdf.format(calendar.getTime()), reason, sender.getName());
-	        if (this.main.getLoaderHandler().getSettings().isMuteBroad()) {
-	        	Bukkit.broadcastMessage(this.main.getLoaderHandler().getMessage().getMuteAnnounce().replace("%muted%", args[0]).replace("%reason%", reason).replace("%judge%", sender.getName()));
-	        }
+		if (args.length == 1) {
+			this.main.getLoaderHandler().getMessage().getMuteHelp().forEach(sender::sendMessage);
 			return;
 		}
-		if (cmd.getName().equalsIgnoreCase("unmute")) {
-			if (!sender.hasPermission(this.main.getLoaderHandler().getPermission().getRemoveBan())) {
-				sender.sendMessage(this.main.getLoaderHandler().getMessage().getNoPermission());
+		if (args[0].equalsIgnoreCase("info") && args.length == 2) {
+			if (!this.main.getManagerHandler().getPunishmentManager().getMuted().containsKey(Bukkit.getPlayer(args[1]) != null ? Bukkit.getPlayer(args[1]).getUniqueId() : Bukkit.getOfflinePlayer(args[1]).getUniqueId())) {
+				sender.sendMessage(this.main.getLoaderHandler().getMessage().getNotExist().replace("%value%", args[1]).replace("%type%", "mute"));
 				return;
 			}
-			if (args.length != 1) {
-				this.main.getLoaderHandler().getMessage().getBanHelp().forEach(sender::sendMessage);
-				return;
-			}
-	        this.main.getManagerHandler().getPunishmentManager().removeMute(Bukkit.getPlayer(args[0]) != null ? Bukkit.getPlayer(args[0]).getUniqueId() : Bukkit.getOfflinePlayer(args[0]).getUniqueId());
-	        if (this.main.getLoaderHandler().getSettings().isMuteBroad()) {
-	        	Bukkit.broadcastMessage(this.main.getLoaderHandler().getMessage().getUnmuteAnnounce().replace("%muted%", args[0]).replace("%judge%", sender.getName()));
-	        }
+			final MuteEntry ban = this.main.getManagerHandler().getPunishmentManager().getMuted().get(Bukkit.getPlayer(args[1]) != null ? Bukkit.getPlayer(args[1]).getUniqueId() : Bukkit.getOfflinePlayer(args[1]).getUniqueId());
+			this.main.getLoaderHandler().getMessage().getPunishmentInfo().forEach(str -> sender.sendMessage(str.replace("%user%", args[1]).replace("%punishmentType%", "MUTE").replace("%expires%", ban.getExpiresOn()).replace("%reason%", ban.getReason()).replace("%judge%", ban.getJudge())));
+			return;
 		}
+    	Calendar calendar = Calendar.getInstance();
+    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    	String reason = "Flood";
+		new TimeUtils(args[1], calendar);
+        reason = args.length > 2 ? StringUtils.join(args, ' ', 2, args.length) : "Flood";  
+        this.main.getManagerHandler().getPunishmentManager().addMute(Bukkit.getPlayer(args[0]) != null ? Bukkit.getPlayer(args[0]).getUniqueId() : Bukkit.getOfflinePlayer(args[0]).getUniqueId(), sdf.format(calendar.getTime()), reason, sender.getName());
+        if (this.main.getLoaderHandler().getSettings().isMuteBroad()) {
+        	Bukkit.broadcastMessage(this.main.getLoaderHandler().getMessage().getMuteAnnounce().replace("%muted%", args[0]).replace("%reason%", reason).replace("%judge%", sender.getName()));
+        }
 		return;
 	}
 
+	@Command(name = "unmute", inGameOnly = false)
+	public void unmuteCommand(final CommandArgs arg) {
+		final CommandSender sender = arg.getSender();
+		final String[] args = arg.getArgs();
+		if (!sender.hasPermission(this.main.getLoaderHandler().getPermission().getRemoveBan())) {
+			sender.sendMessage(this.main.getLoaderHandler().getMessage().getNoPermission());
+			return;
+		}
+		if (args.length != 1) {
+			this.main.getLoaderHandler().getMessage().getMuteHelp().forEach(sender::sendMessage);
+			return;
+		}
+        this.main.getManagerHandler().getPunishmentManager().removeMute(Bukkit.getPlayer(args[0]) != null ? Bukkit.getPlayer(args[0]).getUniqueId() : Bukkit.getOfflinePlayer(args[0]).getUniqueId());
+        if (this.main.getLoaderHandler().getSettings().isMuteBroad()) {
+        	Bukkit.broadcastMessage(this.main.getLoaderHandler().getMessage().getUnmuteAnnounce().replace("%muted%", args[0]).replace("%judge%", sender.getName()));
+        }
+	}
 }
