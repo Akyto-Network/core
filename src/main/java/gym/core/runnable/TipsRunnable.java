a9495a94cb2fad88a7e59912bd17dc78cbd5e011
solvictor <47831936+VictorEgret@users.noreply.github.com>
2024-06-04 19:43:20 +0200

    Better tips

--- src/main/java/gym/core/runnable/TipsRunnable.java
+++ src/main/java/gym/core/runnable/TipsRunnable.java
@@ -4,28 +4,24 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TipsRunnable extends BukkitRunnable {
-	
-	private int counter;
-	private boolean first = true;
+
+	private int idx = 0;
+	private final String prefix = ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Tips" + ChatColor.GRAY + "] " + ChatColor.RESET;
+	private final List<String> messages = Arrays.asList(
+			prefix + "Making C.P.S too high can result in a ban so avoid :p",
+			prefix + "Don't forget to join our discord server " + ChatColor.RED + "discord.akyto.club",
+			prefix + "Vote for us on NameMC: " + ChatColor.RED + "namemc.com/server/akyto.club",
+			prefix + "The server is still under development if you see any bugs report them to us and don't abuse them :3"
+	);
 
 	@Override
 	public void run() {
-		counter++;
-		if (first) {
-			Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Tips" + ChatColor.GRAY + "] " + ChatColor.WHITE + "Making C.P.S too high can result in a ban so avoid :p"));
-			first = false;
-		}
-		if (counter == 1) {
-			Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Tips" + ChatColor.GRAY + "] " + ChatColor.WHITE + "Don't forget to join our discord server " + ChatColor.RED + "discord.akyto.club"));
-		}
-		if (counter == 2) {
-			Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Tips" + ChatColor.GRAY + "] " + ChatColor.WHITE + "Vote for us on NameMC: " + ChatColor.RED + "namemc.com/server/akyto.club"));
-		}
-		if (counter == 3) {
-			Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Tips" + ChatColor.GRAY + "] " + ChatColor.WHITE + "The server is still under development if you see any bugs report them to us and don't abuse them :3"));
-			counter = 0;
-		}
+		Bukkit.getOnlinePlayers().forEach(player -> player.sendMessage(messages.get(idx)));
+		idx++;
 	}
 	
 	
