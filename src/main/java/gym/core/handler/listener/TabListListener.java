c9113a28cc2e1f4962ac0d60ffb96698c9e0a131
solvictor <47831936+VictorEgret@users.noreply.github.com>
2024-06-13 01:14:39 +0200

    tablist WIP

--- src/main/java/gym/core/handler/listener/TabListListener.java
+++ src/main/java/gym/core/handler/listener/TabListListener.java
@@ -1,17 +1,57 @@
 package gym.core.handler.listener;
 
 import akyto.spigot.handler.PacketHandler;
+import com.mojang.authlib.GameProfile;
+import gym.core.Core;
+import net.minecraft.server.v1_8_R3.WorldSettings;
+import net.minecraft.server.v1_8_R3.ChatComponentText;
 import net.minecraft.server.v1_8_R3.Packet;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.UUID;
 
 public class TabListListener implements PacketHandler {
+
+    static {
+        UUID fakeUUID = UUID.fromString("ffffffff-ffff-ffff-ffff-ffffffffffff");
+        if (Core.API.isDebug()) {
+            Core.API.getServer().getPluginManager().registerEvents(new Listener() {
+                @EventHandler
+                public void onJoin(PlayerJoinEvent event) {
+                    Player player = event.getPlayer();
+                    PacketPlayOutPlayerInfo packet = new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER);
+                    for (int i = 0; i < 6; i++) {
+
+                        ChatColor color = PacketPlayOutPlayerInfo.ConnectionBars.values()[i].asColor();
+                        String col = ChatColor.translate(String.valueOf(color));
+                        packet.addFakePlayer(
+                                new GameProfile(
+                                        fakeUUID,
+                                        "TabListFake"
+                                ),
+                                PacketPlayOutPlayerInfo.ConnectionBars.values()[i].asPing(),
+                                WorldSettings.EnumGamemode.SURVIVAL,
+                                new ChatComponentText(col + "Test " + i)
+                        );
+
+                    }
+                    player.sendPacket(packet);
+                }
+            }, Core.API);
+        }
+    }
 
     @Override
     public void handleReceivedPacket(PlayerConnection connection, Packet packet) {
         if (packet instanceof PacketPlayOutPlayerInfo) {
             PacketPlayOutPlayerInfo playerInfo = (PacketPlayOutPlayerInfo) packet;
-            System.out.println("Received " + playerInfo);
+//            System.out.println("Received " + playerInfo);
         }
     }
 
@@ -19,7 +59,7 @@ public class TabListListener implements PacketHandler {
     public void handleSentPacket(PlayerConnection connection, Packet packet) {
         if (packet instanceof PacketPlayOutPlayerInfo) {
             PacketPlayOutPlayerInfo playerInfo = (PacketPlayOutPlayerInfo) packet;
-            System.out.println("Sent " + playerInfo);
+//            System.out.println("Sent " + playerInfo);
         }
     }
 }
